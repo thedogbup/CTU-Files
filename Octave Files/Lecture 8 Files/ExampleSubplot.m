d8b40c4de37198a83e34f4d73995a85c1b19e401
bup <50534745+thedogbup@users.noreply.github.com>
2020-08-05 16:22:54 -0400

    Update ExampleSubplot.m

--- Octave Files/Lecture 8 Files/ExampleSubplot.m
+++ Octave Files/Lecture 8 Files/ExampleSubplot.m
@@ -4,12 +4,17 @@
 %| bup                        |
 %| 8/4/2020                   |
 %|____________________________|
+close all;
 
 x = linspace(1, 2 * pi, 32);
 y1 = sin(x);
 y2 = cos(x);
 y3 = y1 + y2;
 
+f1 = figure('Name', 'sin(x) and cos(x) Subplot');
+f2 = figure('Name', 'tan(x) Plot');
+
+figure(f1)
 subplot(2, 1, 1)
 hold on;
 plot(x, y1)
@@ -20,3 +25,7 @@ subplot(2, 1, 2)
 plot(x, y3)
 legend('sin(x) + cos(x)', 'Location', 'southeast')
 legend('boxoff')
+
+figure(f2)
+hold on;
+plot(x, tan(x))
